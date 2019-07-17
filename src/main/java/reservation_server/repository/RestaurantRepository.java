6c96812f26630a53a24894fd79de15d748e32015
Bakhodir <bakhodir9293@gmail.com>
2019-07-17 10:26:07 -0500

    restaurant and dependent relationships

--- src/main/java/reservation_server/repository/RestaurantRepository.java
+++ src/main/java/reservation_server/repository/RestaurantRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import reservation_server.domain.Restaurant;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 @Repository
@@ -16,6 +15,6 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
             " join fetch r.diningTables as tab " +
             " join fetch tab.times as tim " +
             " where r.address.state like %:state% " +
-            " and tim.date = :date and tim.endTime < :time")
-    Set<Restaurant> findAll(String state, Date date, Date time);
+            " and tim.date = :date and tim.endTime <= :startTime")
+    Set<Restaurant> findAll(String state, Date date, Date startTime);
 }
